5669e2b2437848fcbab9940ca1c05a78da62896d
Ted M. Young <tedyoung@gmail.com>
2019-10-16 16:26:03 -0700

    S03E06 - Refactoring
    Moved the "feature envy" smelling property loading code
    into TwitchProperties.

--- src/main/java/com/jitterted/jittershout/JitterShoutOutBot.java
+++ src/main/java/com/jitterted/jittershout/JitterShoutOutBot.java
@@ -7,22 +7,14 @@ import com.github.twitch4j.TwitchClientBuilder;
 import com.github.twitch4j.chat.TwitchChat;
 import com.github.twitch4j.chat.events.CommandEvent;
 import com.github.twitch4j.chat.events.channel.ChannelMessageEvent;
+import com.github.twitch4j.common.events.channel.ChannelGoLiveEvent;
 import com.github.twitch4j.kraken.TwitchKraken;
 import com.github.twitch4j.kraken.TwitchKrakenBuilder;
 import com.github.twitch4j.kraken.domain.KrakenTeam;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Properties;
-
 @Slf4j
 public class JitterShoutOutBot {
-
-  private static final String TWITCH_API_OAUTH_ACCESS_TOKEN_PROPERTY_KEY = "TWITCH_OAUTH_TOKEN";
-  private static final String TWITCH_API_CLIENT_ID_PROPERTY_KEY = "TWITCH_API_CLIENT_ID";
-  private static final String TWITCH_API_CLIENT_SECRET_PROPERTY_KEY = "TWITCH_API_CLIENT_SECRET";
-  private static final String TWITCH_BOT_NICKNAME = "TWITCH_BOT_NICKNAME";
 
   private static final String CHANNEL_NAME = "jitterted";
   private static final String TEAM_NAME = "livecoders";
@@ -32,37 +24,16 @@ public class JitterShoutOutBot {
   private BotCommandHandler botCommandHandler;
 
   public static void main(String[] args) {
-    TwitchProperties twitchProperties = loadProperties();
+    TwitchProperties twitchProperties = TwitchProperties.loadProperties();
     JitterShoutOutBot jitterShoutOutBot = new JitterShoutOutBot();
     jitterShoutOutBot.connect(twitchProperties);
   }
 
-  public static TwitchProperties loadProperties() {
-    Properties properties = new Properties();
-    try {
-      properties.load(new FileReader("/Users/ted/.twitch.properties"));
-      String oAuthToken = getValidPropertyFrom(properties, TWITCH_API_OAUTH_ACCESS_TOKEN_PROPERTY_KEY);
-      String twitchClientId = getValidPropertyFrom(properties, TWITCH_API_CLIENT_ID_PROPERTY_KEY);
-      String twitchClientSecret = getValidPropertyFrom(properties, TWITCH_API_CLIENT_SECRET_PROPERTY_KEY);
-      String twitchBotNickname = getValidPropertyFrom(properties, TWITCH_BOT_NICKNAME);
-      return new TwitchProperties(oAuthToken, twitchClientId, twitchClientSecret, twitchBotNickname);
-    } catch (IOException e) {
-      log.error("Could not read .twitch.properties file", e);
-      System.exit(-1);
-    }
-    throw new IllegalStateException();
-  }
-
-  private static String getValidPropertyFrom(Properties properties, String key) throws IOException {
-    String property = properties.getProperty(key);
-    if (property == null) {
-      throw new IOException("Could not find property key '" + key + "' in properties file.");
-    }
-    return property;
-  }
-
   public void connect(TwitchProperties twitchProperties) {
     TwitchClient twitchClient = createTwitchClient(twitchProperties);
+
+    twitchClient.getClientHelper()
+                .enableStreamEventListener(CHANNEL_NAME);
 
     TwitchKraken twitchKraken = TwitchKrakenBuilder.builder()
                                                    .withClientId(twitchProperties.getTwitchClientId())
@@ -92,15 +63,23 @@ public class JitterShoutOutBot {
     chat.joinChannel(CHANNEL_NAME);
     chat.sendMessage(CHANNEL_NAME, "The JitterChat ShoutBot is here!");
 
-    EventManager chatEventManager = chat.getEventManager();
+    EventManager eventManager = chat.getEventManager();
 
-    chatEventManager
+    eventManager
         .onEvent(ChannelMessageEvent.class)
         .subscribe(this::onChannelMessage);
 
-    chatEventManager
+    eventManager
         .onEvent(CommandEvent.class)
         .subscribe(this::onCommand);
+
+    eventManager
+        .onEvent(ChannelGoLiveEvent.class)
+        .subscribe(this::onChannelGoLive);
+  }
+
+  private void onChannelGoLive(ChannelGoLiveEvent channelGoLiveEvent) {
+    // integrate with reload of team list
   }
 
   private void onCommand(CommandEvent commandEvent) {
@@ -120,6 +99,7 @@ public class JitterShoutOutBot {
                               .withClientId(twitchProperties.getTwitchClientId())
                               .withClientSecret(twitchProperties.getTwitchClientSecret())
                               .withEnableChat(true)
+                              .withEnableHelix(true)
                               .withChatAccount(credential)
                               .withCommandTrigger("!")
                               .build();
--- src/main/java/com/jitterted/jittershout/TwitchProperties.java
+++ src/main/java/com/jitterted/jittershout/TwitchProperties.java
@@ -1,11 +1,46 @@
 package com.jitterted.jittershout;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
 @Data
+@Slf4j
 public class TwitchProperties {
+  private static final String TWITCH_API_OAUTH_ACCESS_TOKEN_PROPERTY_KEY = "TWITCH_OAUTH_TOKEN";
+  private static final String TWITCH_API_CLIENT_ID_PROPERTY_KEY = "TWITCH_API_CLIENT_ID";
+  private static final String TWITCH_API_CLIENT_SECRET_PROPERTY_KEY = "TWITCH_API_CLIENT_SECRET";
+  private static final String TWITCH_BOT_NICKNAME = "TWITCH_BOT_NICKNAME";
+
   private final String oAuthToken;
   private final String twitchClientId;
   private final String twitchClientSecret;
   private final String twitchBotNickname;
+
+  public static TwitchProperties loadProperties() {
+    Properties properties = new Properties();
+    try {
+      properties.load(new FileReader("/Users/ted/.twitch.properties"));
+      String oAuthToken = getValidPropertyFrom(properties, TWITCH_API_OAUTH_ACCESS_TOKEN_PROPERTY_KEY);
+      String twitchClientId = getValidPropertyFrom(properties, TWITCH_API_CLIENT_ID_PROPERTY_KEY);
+      String twitchClientSecret = getValidPropertyFrom(properties, TWITCH_API_CLIENT_SECRET_PROPERTY_KEY);
+      String twitchBotNickname = getValidPropertyFrom(properties, TWITCH_BOT_NICKNAME);
+      return new TwitchProperties(oAuthToken, twitchClientId, twitchClientSecret, twitchBotNickname);
+    } catch (IOException e) {
+      log.error("Could not read .twitch.properties file", e);
+      System.exit(-1);
+    }
+    throw new IllegalStateException();
+  }
+
+  private static String getValidPropertyFrom(Properties properties, String key) throws IOException {
+    String property = properties.getProperty(key);
+    if (property == null) {
+      throw new IOException("Could not find property key '" + key + "' in properties file.");
+    }
+    return property;
+  }
 }
