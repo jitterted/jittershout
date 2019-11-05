e7f4cf8f0b93ba1231e008a046051dea62a83721
Ted M. Young <tedyoung@gmail.com>
2019-11-05 13:14:28 -0800

    Episode 14: Refactored out the JS code into its own file as well as the app-specific CSS.

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,73 @@
+Vue.component('Toggle', {
+  template: `
+    <span class="toggle-wrapper" @click="toggle" role="checkbox" :aria-checked="value.toString()" tabindex="0" @keydown.space.prevent="toggle">
+      <span class="toggle-background" :style="backgroundStyles"></span>
+      <span class="toggle-indicator" :style="indicatorStyles"></span>
+    </span>
+  `,
+  props: ['value'],
+  methods: {
+    toggle() {
+      this.$emit('input', !this.value)
+    }
+  },
+  computed: {
+    backgroundStyles() {
+      return {
+        backgroundColor: this.value ? '#6059dc' : '#ded9e7'
+      }
+    },
+    indicatorStyles() {
+      return {transform: this.value ? 'translateX(2rem)' : 'translateX(0)'}
+    }
+  }
+});
+
+const axiosApi = axios.create({
+  baseURL: '/api/'
+});
+
+new Vue({
+  el: '#app',
+  data: {
+    team: null,
+    bot: {shoutOutActive: false},
+    loading: true,
+    errored: false
+  },
+  mounted() {
+    this.loadTeamInfo();
+    this.loadBotStatus();
+  },
+  methods: {
+    updateShoutOutState: function (event) {
+      axiosApi
+        .post('botinfo', this.bot)
+        .then(response => (console.log(response)))
+        .catch(error => (console.log(error)));
+    },
+    refreshTeam: function (event) {
+      axiosApi
+        .post('refresh-team')
+        .then(response => (this.loadTeamInfo()))
+        .catch(error => (console.log(error)));
+    },
+    loadTeamInfo: function () {
+      this.loading = true;
+      axiosApi
+        .get('teaminfo')
+        .then(response => (this.team = response.data))
+        .catch(error => {
+          console.log(error);
+          this.errored = true;
+        })
+        .finally(() => this.loading = false);
+    },
+    loadBotStatus: function () {
+      axiosApi
+        .get('botinfo')
+        .then(response => (this.bot = response.data))
+        .catch(error => (console.log(error)));
+    }
+  }
+});
